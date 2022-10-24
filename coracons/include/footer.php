189090f77c6009edb22341e08b3604637604c47f
Felice <felice.coraci@gmail.com>
2022-10-24 17:52:40 +0200

    first commit

---
+++ coracons/include/footer.php
@@ -0,0 +1,5 @@
+<div align="center" style="background-color: #0000FF; padding: 1px;" name="footer" id="footer">
+
+<p align="center" style="font-size: 9pt; color: white; text-align: center; ">Coraci Consulenze Partita IVA 03142090541  -  Sede legale: 06081 Assisi Via Betto di Menico 1 || Copyright &copy; 2022 APP CoraCons Dashboard . Created byfelice - <a href="https://coraciconsulenze.it" target="_blank" title="Visita il nostro blog">Coraci Consulenze</a></p>
+
+</div>
---
+++ coracons/include/header_small.php~
@@ -0,0 +1,7 @@
+<div align="center" style="background-color: #E6E6FA; height: 50px; padding: 5px;" class="headersmall" name="header_small">
+
+ <a href="../index.php" target="_top" title="Torna alla Home page"><img src="../img/coraci-consulenze1.png" width="103" height="51" style="margin-left: 12%; " alt="coraci consulenze logo" align="left"></a>
+ 
+ <h5><b>Coraci Consulenze.</b> Area archivi e gestione clientela</h5>
+
+</div>
